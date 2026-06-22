0f5123bcf808f64b2fc1ef4d3e793ee3c5b596bb
BALAJIVASUN <balajivasun@gmail.com>
2026-06-22 15:01:16 +0530

    Add TestDatabase class to demonstrate singleton pattern

---
+++ Design Patterns and Principles/TestDatabase.java
@@ -0,0 +1,12 @@
+public class TestDatabase {
+    public static void main(String[] args) {
+        DatabaseConnection d1 = DatabaseConnection.getInstance();
+        DatabaseConnection d2 = DatabaseConnection.getInstance();
+        d1.connect();
+        if (d1 == d2) {
+            System.out.println("Only one object created");
+        } else {
+            System.out.println("Multiple objects created");
+        }
+    }
+}
