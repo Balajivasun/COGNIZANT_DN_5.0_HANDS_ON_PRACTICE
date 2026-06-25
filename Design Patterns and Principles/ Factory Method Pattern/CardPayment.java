d91b8b54fbfa2fe2b470e7ad980a1d73a701b5ee
BALAJIVASUN <balajivasun@gmail.com>
2026-06-25 16:00:37 +0530

    Refactored folder structure for better organization

--- Design Patterns and Principles/ Factory Method Pattern/CardPayment.java
+++
@@ -1,6 +0,0 @@
-public class CardPayment implements Payment {
-
-    public void pay() {
-        System.out.println("Payment done using Card");
-    }
-}
--- Design Patterns and Principles/ Factory Method Pattern/UPIPayment.java
+++
@@ -1,6 +0,0 @@
-public class UPIPayment implements Payment {
-
-    public void pay() {
-        System.out.println("Payment done using UPI");
-    }
-}
